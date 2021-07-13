3e6c3162933a9c891c0431d715da27f4422d7a06
kivimann <arvi.kivimannila@gmail.com>
2021-07-13 15:51:34 +0300

    test1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -6,6 +6,7 @@ namespace PartsUnlimited.Models
     public class CartItem : ILineItem
     {
         //my change fourth change
+        //muutos
         [Key]
         public int CartItemId { get; set; }
 
